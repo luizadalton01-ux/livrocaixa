3b8b88dab595061259c16cc7ee6fc225dfbb0bfc
luizadalton01-ux <luizadalton01@gmail.com>
2026-08-19 20:16:12 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@
  * novo, evitando que o usuário fique preso numa versão desatualizada do app.
  */
 
-const CACHE_VERSION = 'lc-cache-v1.34.11';
+const CACHE_VERSION = 'lc-cache-v1.34.12';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
